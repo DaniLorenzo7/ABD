41eeed5b00fa346d17501e998bad48a2e0623b72
Irene Domínguez Estévez <irene.dominguez.estevez@udc.es>
2020-04-11 14:43:13 +0200

    generadas tablas de hoteles, reservas y habitaciones

--- prueba.py
+++ prueba.py
@@ -107,57 +107,71 @@ j=0
 
 
 ################# HOTEL #################
-# while j<127:
-#     localizacion = np.random.choice(localizaciones)
-#     aux_aforo_max = random.randint(100,400)
-#     aforo_max = str(aux_aforo_max)
-#     aux_evento = random.choices(population=eventos_cocina, weights=pesos_eventos, k=1)
-#     evento = ''.join(aux_evento)
+while j<127:
+    localizacion = np.random.choice(localizaciones)
+    aux_aforo_max = random.randint(100,400)
+    aforo_max = str(aux_aforo_max)
+    aux_evento = random.choices(population=eventos_cocina, weights=pesos_eventos, k=1)
+    evento = ''.join(aux_evento)
+    director = random.randint(1,120)
 
-#     print(localizacion,(20-len(localizacion))*" ",
-#         aforo_max,12*" ",
-#         evento,(15-len(evento))*" ",
-#         )
+    print(localizacion,(20-len(localizacion))*" ",
+        aforo_max,12*" ",
+        evento,(15-len(evento))*" ",
+        director,(10-len(str(director)))*" "
+        )
 
-#     j+=1
+    j+=1
 
 
 
 ################# HABITACION #################
+# id_hotel=0
 # while j<6000:
-#     aux_tipo_habitacion = random.choices(population=tipos_habitaciones, weights=pesos_tipos_habitaciones, k=1)
-#     tipo_habitacion = ''.join(aux_tipo_habitacion)
-#     aux_tipo_baño = random.choices(population=tipos_baños, weights=pesos_tipos_baños, k=1)
-#     tipo_baño = ''.join(aux_tipo_baño)
-#     aux_cocina = random.choices(population=eventos_cocina, weights=pesos_cocina, k=1)
-#     cocina = ''.join(aux_cocina)
-
-#     print(tipo_habitacion,(15-len(tipo_habitacion))*" ",
+#     id_hotel+=1
+#     num_habitaciones=[20, 30, 40, 50, 60, 70, 80];
+#     pesos_habitaciones=[0.1,0.15,0.2,0.2,0.2,0.1,0.05]
+#     aux=random.choices(population=num_habitaciones, weights=pesos_habitaciones, k=1)
+#     i=0
+#     if (id_hotel==127):
+#         aux[0]=6000-j
+#     while (i<aux[0] and j<6000):
+#         aux_tipo_habitacion = random.choices(population=tipos_habitaciones, weights=pesos_tipos_habitaciones, k=1)
+#         tipo_habitacion= ''.join(aux_tipo_habitacion)
+#         aux_tipo_baño = random.choices(population=tipos_baños, weights=pesos_tipos_baños, k=1)
+#         tipo_baño = ''.join(aux_tipo_baño)
+#         aux_cocina = random.choices(population=eventos_cocina, weights=pesos_cocina, k=1)
+#         cocina = ''.join(aux_cocina)
+#         print(tipo_habitacion,(15-len(tipo_habitacion))*" ",
 #         tipo_baño,(10-len(tipo_baño))*" ",
 #         cocina,12*" ",
+#         id_hotel,(7-len(str(id_hotel)))*" "
 #         )
-
-#     j+=1
+#         i+=1
+#         j+=1
 
 
 ################# RESERVA #################
-while j<12000:
-    aux_fecha_inicio = inicio + (final - inicio) * random.random()
-    aux_fecha_inicio_1= str(aux_fecha_inicio.day),"-",str(aux_fecha_inicio.month),"-",str(aux_fecha_inicio.year)
-    fecha_inicio= ''.join(aux_fecha_inicio_1)
+# while j<12000:
+#     aux_fecha_inicio = inicio + (final - inicio) * random.random()
+#     aux_fecha_inicio_1= str(aux_fecha_inicio.day),"-",str(aux_fecha_inicio.month),"-",str(aux_fecha_inicio.year)
+#     fecha_inicio= ''.join(aux_fecha_inicio_1)
 
-    duracion= random.randint(1,15)
+#     duracion= random.randint(1,15)
 
-    aux_fecha_fin = aux_fecha_inicio + datetime.timedelta(days=duracion)
-    aux_fecha_fin_1= str(aux_fecha_fin.day),"-",str(aux_fecha_fin.month),"-",str(aux_fecha_fin.year)
-    fecha_fin= ''.join(aux_fecha_fin_1)
+#     aux_fecha_fin = aux_fecha_inicio + datetime.timedelta(days=duracion)
+#     aux_fecha_fin_1= str(aux_fecha_fin.day),"-",str(aux_fecha_fin.month),"-",str(aux_fecha_fin.year)
+#     fecha_fin= ''.join(aux_fecha_fin_1)
 
-    aux_regimen = random.choices(population=regimenes, weights=pesos_regimenes, k=1)
-    regimen = ''.join(aux_regimen)
+#     aux_regimen = random.choices(population=regimenes, weights=pesos_regimenes, k=1)
+#     regimen = ''.join(aux_regimen)
 
-    print(fecha_inicio,(15-len(fecha_inicio))*" ",
-        fecha_fin,(15-len(fecha_fin))*" ",
-        regimen,(15-len(regimen))*" ",
-        )
+#     id_habitacion = random.randint(1,6000)
 
-    j+=1
+#     print(id_habitacion, (7-len(str(id_habitacion)))*" ",
+#         fecha_inicio,(15-len(fecha_inicio))*" ",
+#         fecha_fin,(15-len(fecha_fin))*" ",
+#         regimen,(15-len(regimen))*" ",
+#         )
+
+#     j+=1
